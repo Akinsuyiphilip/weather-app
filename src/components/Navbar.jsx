6a31d27305991a5d10b43e11ebb46548690a4d03
Akinsuyiphilip <philipakinbolade@gmail.com>
2022-05-30 15:12:37 +0100

    links updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ function Navbar() {
                <i className={ click ? 'fas fa-times' : 'fas fa-bars'}></i>
            </div>
         <div className={ click ? 'nav-menu active' : 'nav-menu '}>
-            <a href="#" > Github <i class="fa-brands fa-github"></i></a>
-            <a href="#" >Twitter <i class="fa-brands fa-twitter"></i></a>
-            <a href="#" >facebook <i class="fa-brands fa-facebook"></i></a>
-            <a href="#" >Instagram <i class="fa-brands fa-instagram"></i></a>
-            <a href="#" >Youtube <i class="fa-brands fa-youtube"></i></a>
+            <a href="https://github.com/Akinsuyiphilip" > Github <i class="fa-brands fa-github"></i></a>
+            <a href="https://twitter.com/june_of_lagos" >Twitter <i class="fa-brands fa-twitter"></i></a>
+            <a href="https://web.facebook.com/profile.php?id=100070730607040" >facebook <i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.instagram.com/juneoflagos/" >Instagram <i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.youtube.com/channel/UC6N085Nmf0ulGr9pHwe5TVw" >Youtube <i class="fa-brands fa-youtube"></i></a>
         </div>
     </nav>
 </>
